494a3ed37d8590ba7a4762e80fab169fd56d3ff6
Kirill.Galimov <mr.galimov91@gmail.com>
2022-06-17 12:54:59 +0300

    add test

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,5 +1,9 @@
-import { getTrue } from './utils';
+import { getFalse, getTrue } from './utils';
 
 it('should return true', () => {
   expect(getTrue()).toBeTruthy();
+});
+
+it('should return false', () => {
+  expect(getFalse()).toBeFalsy();
 });
