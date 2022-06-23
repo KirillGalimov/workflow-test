9e2a881b07a09eaf4f1e5c3577d0fdeee8621abd
Kirill.Galimov <mr.galimov91@gmail.com>
2022-06-23 16:37:48 +0300

    test

--- src/utils.test.js
+++ src/utils.test.js
@@ -3,7 +3,3 @@ import { getTrue, getFalse, getNull } from './utils';
 it('should return true', () => {
   expect(getTrue()).toBeTruthy();
 });
-
-it('should return false', () => {
-  expect(getFalse()).toBeFalsy();
-});
